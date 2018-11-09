11c6dd256a8df16684318313bbae88df5556d04d
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-11-09 18:13:02 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO burgers (burger_name)
+VALUES ('Big Mac');
+INSERT INTO burgers (burger_name)
+VALUES ('Whopper');
+INSERT INTO burgers (burger_name)
+VALUES ('Baconator');
+INSERT INTO burgers (burger_name)
+VALUES ('McDouble');
+INSERT INTO burgers (burger_name)
+VALUES ('Veggie Burger');
+
+SELECT * FROM burgers;
+
