64199b0a9c7af8a3bfbb58de464c3a7c3b027ac9
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-11-17 22:01:58 -0500

    updated with sequelize ORM

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 8080;
-
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var PORT = process.env.PORT || 9000;
+
+// Requiring our models for syncing
+var db = require("./models");
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -18,10 +21,17 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
+// var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+// Routes
+// =============================================================
+require("./controllers/burgers_controller.js")(app);
+// app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+  });
 });
